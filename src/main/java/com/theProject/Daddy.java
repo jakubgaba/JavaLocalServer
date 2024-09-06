b638adaa32506c8d42c484b30b73f66ba5ea2d9e
Jakub <j.gaba@email.cz>
2024-09-06 21:42:53 +0200

    ServerRunsOnDiffThreadButItsKindaNotSafeIQuess

--- src/main/java/com/theProject/Daddy.java
+++ src/main/java/com/theProject/Daddy.java
@@ -6,9 +6,16 @@ import com.theProject.service.Server;
 public class Daddy {
     public static void main(String[] args) {
         int port = 8080;
+
+        // Start the server in a new thread
+        Thread serverThread = new Thread(() -> {
+            Server server = new Server(port);
+            server.start();
+        });
+
+        serverThread.start();  // Start the server thread
+
+        // Launch the JavaFX application on the JavaFX Application Thread
         MyPrettyGUI.launch(MyPrettyGUI.class, args);
-        Server server = new Server(port);
-        server.start();
- 
     }
 }
