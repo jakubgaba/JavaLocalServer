af8eef668d1d98f1107b131e1599b04eaa2dd7bb
Jakub <j.gaba@email.cz>
2024-09-09 19:08:46 +0200

    SomethingsIsNotRight

--- src/main/java/com/theProject/Daddy.java
+++ src/main/java/com/theProject/Daddy.java
@@ -1,19 +1,18 @@
 package com.theProject;
 
 import com.theProject.GUI.MyPrettyGUI;
-import com.theProject.service.Server;
 
 public class Daddy {
     public static void main(String[] args) {
         int port = 8080;
 
-        // Start the server in a new thread
-        Thread serverThread = new Thread(() -> {
-            Server server = new Server(port);
-            server.start();
-        });
+        // // Start the server in a new thread
+        // Thread serverThread = new Thread(() -> {
+        //     Server server = new Server(port);
+        //     server.start();
+        // });
 
-        serverThread.start();  // Start the server thread
+        // serverThread.start();  // Start the server thread
 
         // Launch the JavaFX application on the JavaFX Application Thread
         MyPrettyGUI.launch(MyPrettyGUI.class, args);
